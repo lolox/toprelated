56c7b7e661bd442299e24a60d8fbc2505762148c
lolox <pantumaco@gmail.com>
2016-04-19 23:30:54 +0200

    Fichero modificado para exteneder funcionamiento del server

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+//Lets require/import the HTTP module
 var http = require('http');
 
-http.createServer(function(request, response) {
-  var body = [];
-  request.on('data', function(chunk) {
-    body.push(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-    response.end(body);
-  });
-}).listen(8080);
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
